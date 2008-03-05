d813d8b65308702beb617996ca97ade587e0af16
takscape <takscape@6596db54-5b47-0410-be49-79595e049135>
2008-03-05 14:06:58 +0000

    ソースコードにヘッダ追加

--- src/net/moraleboost/lucene/analysis/ja/MeCabAnalyzer.java
+++ src/net/moraleboost/lucene/analysis/ja/MeCabAnalyzer.java
@@ -1,3 +1,19 @@
+/*
+**
+**  Mar. 5, 2008
+**
+**  The author disclaims copyright to this source code.
+**  In place of a legal notice, here is a blessing:
+**
+**    May you do good and not evil.
+**    May you find forgiveness for yourself and forgive others.
+**    May you share freely, never taking more than you give.
+**
+**                                         Stolen from SQLite :-)
+**  Any feedback is welcome.
+**  Kohei TAKETA <k-tak@void.in>
+**
+*/
 package net.moraleboost.lucene.analysis.ja;
 
 import java.io.Reader;
