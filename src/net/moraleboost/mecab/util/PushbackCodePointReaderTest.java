16e439fa390aa9bf420434ac3c96085964d56d0f
takscape <takscape@6596db54-5b47-0410-be49-79595e049135>
2009-02-16 12:52:37 +0000

    assert時の型の誤りを訂正

--- src/net/moraleboost/mecab/util/PushbackCodePointReaderTest.java
+++ src/net/moraleboost/mecab/util/PushbackCodePointReaderTest.java
@@ -20,13 +20,13 @@ public class PushbackCodePointReaderTest
 		CodePointReader base = new BasicCodePointReader(car);
 		PushbackCodePointReader reader = new PushbackCodePointReader(base, 2);
 		
-		assertEquals((int)'a', reader.read()); assertEquals(1, reader.getPosition());
-		assertEquals((int)'b', reader.read()); assertEquals(2, reader.getPosition());
-		assertEquals((int)'c', reader.read()); assertEquals(3, reader.getPosition());
-		reader.unread((int)'あ', 1); assertEquals(2, reader.getPosition());
-		reader.unread(scp, 2); assertEquals(0, reader.getPosition());
-		assertEquals(scp, reader.read()); assertEquals(2, reader.getPosition());
-		assertEquals((int)'あ', reader.read()); assertEquals(3, reader.getPosition());
+		assertEquals((int)'a', reader.read()); assertEquals(1L, reader.getPosition());
+		assertEquals((int)'b', reader.read()); assertEquals(2L, reader.getPosition());
+		assertEquals((int)'c', reader.read()); assertEquals(3L, reader.getPosition());
+		reader.unread((int)'あ', 1); assertEquals(2L, reader.getPosition());
+		reader.unread(scp, 2); assertEquals(0L, reader.getPosition());
+		assertEquals(scp, reader.read()); assertEquals(2L, reader.getPosition());
+		assertEquals((int)'あ', reader.read()); assertEquals(3L, reader.getPosition());
 		assertEquals(-1, reader.read());
 	}
 	
